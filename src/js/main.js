bd882b187c4578575c4380a91d31f4a8a2f8222b
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-04-15 23:54:26 +0430

    Fix caricature and click animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,85 @@
-// Powered by (cdpn.io/CBwhg)
-var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy, eyer){
-    $(".caricature-page .eyes").append("<div id='" + eyecontainer + "'><div id='" + pupil + "'></div></div>")
+//When screen bigger 700px
+if($(window).width() >= 700) {
 
-    eyecontainer = "#" + eyecontainer;
-    pupil = "#" + pupil;
+    //Follow Eyes
+    //Powered by (cdpn.io/CBwhg)
+    var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy, eyer){
+        $(".caricature-page .eyes").append("<div id='" + eyecontainer + "'><div id='" + pupil + "'></div></div>")
 
-    // $(eyecontainer).css({left: eyeposx, top:eyeposy});
-    $(eyecontainer).css({marginLeft: eyeposx, marginTop: eyeposy});
-    $(pupil).css({width:eyer*0.4,height:eyer*0.4});
-    $(eyecontainer).css({width:eyer,height:eyer});
-    $(pupil).css({position: 'relative', background: '#000000', 'border-radius':'50%'});
-    $(eyecontainer).css({position:'absolute', background:'#E2DEE9', overflow:'hidden', 'border-radius': '50%'});
-    // $(eyecontainer).css({position:'absolute', border:'1px solid red', overflow:'hidden', 'border-radius': '50%'});
-    // $(eyecontainer).css({position:'absolute', overflow:'hidden', 'border-radius': '50%'});
+        eyecontainer = "#" + eyecontainer;
+        pupil = "#" + pupil;
 
-    // Initialise core variables
-    var r = $(pupil).width()/2;
-    var center = {
-        x: $(eyecontainer).width()/2 - r,
-        y: $(eyecontainer).height()/2 - r
-    };
-    var distanceThreshold = $(eyecontainer).width()/2 - r;
-    var mouseX = center.x, mouseY = center.y;
+        // $(eyecontainer).css({left: eyeposx, top:eyeposy});
+        $(eyecontainer).css({marginLeft: eyeposx, marginTop: eyeposy});
+        $(pupil).css({width:eyer*0.4,height:eyer*0.4});
+        $(eyecontainer).css({width:eyer,height:eyer});
+        $(pupil).css({position: 'relative', background: '#000000', 'border-radius':'50%'});
+        $(eyecontainer).css({position:'absolute', background:'#E2DEE9', overflow:'hidden', 'border-radius': '50%'});
+        // $(eyecontainer).css({position:'absolute', border:'1px solid red', overflow:'hidden', 'border-radius': '50%'});
+        // $(eyecontainer).css({position:'absolute', overflow:'hidden', 'border-radius': '50%'});
 
-    // Listen for mouse movement
-    $(window).mousemove(function(e){
-        // console.log($(eyecontainer).position().left);
-        var d = {
-            x: e.pageX - r - ($(eyecontainer).position().left + eyeposx ) - center.x,
-            y: e.pageY - r - ($(eyecontainer).position().top + eyeposy) - center.y
+        // Initialise core variables
+        var r = $(pupil).width()/2;
+        var center = {
+            x: $(eyecontainer).width()/2 - r,
+            y: $(eyecontainer).height()/2 - r
         };
-        var distance = Math.sqrt(d.x*d.x + d.y*d.y);
-        if (distance < distanceThreshold) {
-            mouseX = e.pageX - eyeposx - r;
-            mouseY = e.pageY - eyeposy - r;
-        } else {
-            mouseX = d.x / distance * distanceThreshold + center.x;
-            mouseY = d.y / distance * distanceThreshold + center.y;
-        }
+        var distanceThreshold = $(eyecontainer).width()/2 - r;
+        var mouseX = center.x, mouseY = center.y;
+
+        // Listen for mouse movement
+        $(window).mousemove(function(e){
+            // console.log($(eyecontainer).position().left);
+            var d = {
+                x: e.pageX - r - ($(eyecontainer).position().left + eyeposx ) - center.x,
+                y: e.pageY - r - ($(eyecontainer).position().top + eyeposy) - center.y
+            };
+            var distance = Math.sqrt(d.x*d.x + d.y*d.y);
+            if (distance < distanceThreshold) {
+                mouseX = e.pageX - eyeposx - r;
+                mouseY = e.pageY - eyeposy - r;
+            } else {
+                mouseX = d.x / distance * distanceThreshold + center.x;
+                mouseY = d.y / distance * distanceThreshold + center.y;
+            }
+        });
+
+        // Update pupil location
+        var pupil = $(pupil);
+
+        var xp = center.x, yp = center.y;
+
+        var loop = setInterval(function(){
+            // change 1 to alter damping/momentum - higher is slower
+            xp += (mouseX - xp) / 2;
+            yp += (mouseY - yp) / 2;
+            pupil.css({left:xp, top:yp});
+        }, 1);
+    };
+    var eyeLeft = new DrawEye("eyeLeft", "pupilLeft", 252, 76.5, 12);
+    var eyeRight = new DrawEye("eyeRight", "pupilRight", 278, 69, 12);
+
+    
+
+
+    //Click animation for caricature When Click on page
+    $( ".caricature-page").click(function() {
+        $( ".caricature-page .click").show();
+        setTimeout(function() {
+            $( ".caricature-page .click").hide();
+        }, 200);
+
     });
 
-    // Update pupil location
-    var pupil = $(pupil);
 
-    var xp = center.x, yp = center.y;
 
-    var loop = setInterval(function(){
-        // change 1 to alter damping/momentum - higher is slower
-        xp += (mouseX - xp) / 2;
-        yp += (mouseY - yp) / 2;
-        pupil.css({left:xp, top:yp});
-    }, 1);
-};
+    //Click animation for caricature every 3 second
+    setInterval(function() {
+        $( ".caricature-page .click").show();
+        setTimeout(function() {
+            $( ".caricature-page .click").hide();
+        }, 200);
+    }, 3000);
+    
+}
 
-var eyeLeft = new DrawEye("eyeLeft", "pupilLeft", 252, 76.5, 12);
-var eyeRight = new DrawEye("eyeRight", "pupilRight", 278, 69, 12);
