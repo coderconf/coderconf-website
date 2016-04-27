7183043e92124dc5937970fe433e8a03f6bb961b
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-04-27 05:05:20 +0430

    Update style, and use new images

--- src/js/main.js
+++ src/js/main.js
@@ -97,9 +97,14 @@ $('.languages .active a').click(function () {
 
 
 
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create','UA-57178802-2','auto');ga('send','pageview');
+
+
+
+
+
+// (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+//     function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+//     e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+//     e.src='https://www.google-analytics.com/analytics.js';
+//     r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+// ga('create','UA-57178802-2','auto');ga('send','pageview');
